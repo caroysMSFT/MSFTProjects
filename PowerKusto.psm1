8ef18803df4e84be8516e4e703ce3e41f77516f8
caroysMSFT <80799592+caroysMSFT@users.noreply.github.com>
2022-06-08 11:21:25 -0500

    Adjusting spacing for readability

--- PowerKusto.psm1
+++ PowerKusto.psm1
@@ -1,78 +1,78 @@
 function execute-kql($clusterUrl, $databaseName, $query)
 {
 
-#following : https://docs.microsoft.com/en-us/azure/data-explorer/kusto/api/powershell/powershell
+    #following : https://docs.microsoft.com/en-us/azure/data-explorer/kusto/api/powershell/powershell
 
-try
-{
-    Add-Type -LiteralPath "$(get-KEPath)\Kusto.Data.dll"
-}
-catch
-{
-  $_.ErrorDetails | get-member
-  $_.Exception.LoaderExceptions
-}
+    try
+    {
+        Add-Type -LiteralPath "$(get-KEPath)\Kusto.Data.dll"
+    }
+    catch
+    {
+      $_.ErrorDetails | get-member
+      $_.Exception.LoaderExceptions
+    }
 
-#   Option A: using Azure AD User Authentication
-$kcsb = New-Object Kusto.Data.KustoConnectionStringBuilder ($clusterUrl, $databaseName)
-$kcsb."AAD Federated Security" = $true
-$queryProvider = [Kusto.Data.Net.Client.KustoClientFactory]::CreateCslQueryProvider($kcsb)
-$queryProvider | Get-Member
+    #   Option A: using Azure AD User Authentication
+    $kcsb = New-Object Kusto.Data.KustoConnectionStringBuilder ($clusterUrl, $databaseName)
+    $kcsb."AAD Federated Security" = $true
+    $queryProvider = [Kusto.Data.Net.Client.KustoClientFactory]::CreateCslQueryProvider($kcsb)
+    $queryProvider | Get-Member
 
-Write-Host "Executing query: '$query' with connection string: '$($kcsb.ToString())'"
-#   Optional: set a client request ID and set a client request property (e.g. Server Timeout)
-$crp = New-Object Kusto.Data.Common.ClientRequestProperties
-$crp.ClientRequestId = "MyPowershellScript.ExecuteQuery." + [Guid]::NewGuid().ToString()
-$crp.SetOption([Kusto.Data.Common.ClientRequestProperties]::OptionServerTimeout, [TimeSpan]::FromSeconds(30))
+    Write-Host "Executing query: '$query' with connection string: '$($kcsb.ToString())'"
+    #   Optional: set a client request ID and set a client request property (e.g. Server Timeout)
+    $crp = New-Object Kusto.Data.Common.ClientRequestProperties
+    $crp.ClientRequestId = "MyPowershellScript.ExecuteQuery." + [Guid]::NewGuid().ToString()
+    $crp.SetOption([Kusto.Data.Common.ClientRequestProperties]::OptionServerTimeout, [TimeSpan]::FromSeconds(30))
 
-$crp.SecurityToken
+    $crp.SecurityToken
 
-#   Execute the query
-$reader = $queryProvider.ExecuteQuery($query, $crp)
+    #   Execute the query
+    $reader = $queryProvider.ExecuteQuery($query, $crp)
 
-# Do something with the result datatable, for example: print it formatted as a table, sorted by the
-# "StartTime" column, in descending order
-$dataTable = [Kusto.Cloud.Platform.Data.ExtendedDataReader]::ToDataSet($reader).Tables[0]
+    # Do something with the result datatable, for example: print it formatted as a table, sorted by the
+    # "StartTime" column, in descending order
+    $dataTable = [Kusto.Cloud.Platform.Data.ExtendedDataReader]::ToDataSet($reader).Tables[0]
 
-return $dataTable
+    return $dataTable
 
 }
 
 function execute-kqlcommand($clusterUrl, $databaseName = "NetDefaultDB", $command)
 {
 
-try
-{
-    Add-Type -LiteralPath "$(get-KEPath)\Kusto.Data.dll"
-}
-catch
-{
-  $_.ErrorDetails | get-member
-  $_.Exception.LoaderExceptions
-}
+    try
+    {
+        Add-Type -LiteralPath "$(get-KEPath)\Kusto.Data.dll"
+    }
+    catch
+    {
+      $_.ErrorDetails | get-member
+      $_.Exception.LoaderExceptions
+    }
 
-#   Option A: using Azure AD User Authentication
-$kcsb = New-Object Kusto.Data.KustoConnectionStringBuilder ($clusterUrl, $databaseName)
-$kcsb."AAD Federated Security" = $true
-$adminProvider = [Kusto.Data.Net.Client.KustoClientFactory]::CreateCslAdminProvider($kcsb)
+    #   Option A: using Azure AD User Authentication
+    $kcsb = New-Object Kusto.Data.KustoConnectionStringBuilder ($clusterUrl, $databaseName)
+    $kcsb."AAD Federated Security" = $true
+    $adminProvider = [Kusto.Data.Net.Client.KustoClientFactory]::CreateCslAdminProvider($kcsb)
 
 
-Write-Host "Executing command: '$query' with connection string: '$($kcsb.ToString())'"
-#   Optional: set a client request ID and set a client request property (e.g. Server Timeout)
-$crp = New-Object Kusto.Data.Common.ClientRequestProperties
-$crp.ClientRequestId = "MyPowershellScript.ExecuteQuery." + [Guid]::NewGuid().ToString()
-$crp.SetOption([Kusto.Data.Common.ClientRequestProperties]::OptionServerTimeout, [TimeSpan]::FromSeconds(30))
+    Write-Host "Executing command: '$query' with connection string: '$($kcsb.ToString())'"
+    #   Optional: set a client request ID and set a client request property (e.g. Server Timeout)
+    $crp = New-Object Kusto.Data.Common.ClientRequestProperties
+    $crp.ClientRequestId = "MyPowershellScript.ExecuteQuery." + [Guid]::NewGuid().ToString()
+    $crp.SetOption([Kusto.Data.Common.ClientRequestProperties]::OptionServerTimeout, [TimeSpan]::FromSeconds(30))
 
-$crp.SecurityToken
+    $crp.SecurityToken
 
-#   Execute the query
-$reader = $adminProvider.ExecuteControlCommand($command, $crp)
+    #   Execute the query
+    $reader = $adminProvider.ExecuteControlCommand($command, $crp)
 
-# Do something with the result datatable, for example: print it formatted as a table, sorted by the
-# "StartTime" column, in descending order
-$dataTable = [Kusto.Cloud.Platform.Data.ExtendedDataReader]::ToDataSet($reader).Tables[0]
+    # Do something with the result datatable, for example: print it formatted as a table, sorted by the
+    # "StartTime" column, in descending order
+    $dataTable = [Kusto.Cloud.Platform.Data.ExtendedDataReader]::ToDataSet($reader).Tables[0]
 
-return $dataTable
+    return $dataTable
 
 }
 
